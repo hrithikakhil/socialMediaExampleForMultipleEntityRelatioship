205159aa1381330f925c79fa499501934def7096
hrithikakhil <hrithikakhil18@gmail.com>
2024-08-24 16:24:06 +0530

    Many to Many example added

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -3,7 +3,9 @@ package com.social.media.models;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class SocialUser {
@@ -18,7 +20,14 @@ public class SocialUser {
 
     @OneToMany(mappedBy = "socialUser")
     // What is this saying?
-    // The below relationship is being managed by POST class and "socialUser" is the field which is managing the field.
+    // The below relationship is being managed by POST class and "socialUser" is the field which is managing the relation.
     private List<Post> posts = new ArrayList<Post>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_group",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "groupd_id") )
+    private Set<SocialGroup> groups = new HashSet<SocialGroup>();
 
 }
